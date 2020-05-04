d06e478a3d39d22f060f651273450a859f7cd1ed
MaGaroo <amoomajid99@gmail.com>
2020-05-04 18:56:25 +0430

    Change DB address format to absolute path

--- src/db.py
+++ src/db.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-engine = create_engine('sqlite:///./default.sqlite3', echo=True)
+engine = create_engine(f'sqlite:////home/{os.getenv("USER")}/.memoz/default.sqlite3', echo=True)
 Base = declarative_base()
 session_gen = sessionmaker(bind=engine)
 
