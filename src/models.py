6edde3a42ccc9b412ae0844f9a53290ffb24e0be
Grace Hawkins <amoomajid99@gmail.com>
2021-02-22 04:16:29 +0330

    Change failure stage of the cards to 0

--- src/models.py
+++ src/models.py
@@ -32,7 +32,7 @@ class Card(Base):
         return '<Card q="{}">'.format(self.question)
 
     def apply_solution(self, correct: bool):
-        self.stage = self.stage + 1 if correct else 1
+        self.stage = self.stage + 1 if correct else 0
         self.ask_date = date.today() + timedelta(days=duration[self.stage])
 
 
