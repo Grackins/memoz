adbed3d6655a1e68421050b32011b25ac9cc1a0b
Grace Hawkins <amoomajid99@gmail.com>
2021-10-16 05:08:19 +0330

    Implement stats view to match pask model

--- src/views/stats.py
+++ src/views/stats.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from .states import STATE_HOME
 from .key_responsed import KeyResponsedView
 from db import session_gen
@@ -8,25 +10,32 @@ class StatsView(KeyResponsedView):
     transitions = [
         (['q'], STATE_HOME, 'Quit'),
     ]
+    powers = [0, 1, 3, 7, 30, 60, 90, 180, 365, 1000, 10**4]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.session = session_gen()
-        self.cards_qs = self.session.query(Card).all()
+        self.cards_qs = self.session.query(Card)
 
     def get_body(self):
-        cnt = [0] * 20
+        cnt = [0 for _ in self.powers]
         for card in self.cards_qs:
-            cnt[card.stage] += 1
+            power = card.get_power()
+            if card.in_queue:
+                power = 0
+            ptr = 0
+            while power > timedelta(self.powers[ptr]):
+                ptr += 1
+            cnt[ptr] += 1
         while cnt[-1] == 0:
             cnt.pop()
 
-        header_line = ' Stage '
+        header_line = ' Power '
         sep_line = '       '
         count_line = '   #   '
         field_format = '{:^5d}'
 
-        for i, c in enumerate(cnt):
+        for i, c in zip(self.powers, cnt):
             header_line += '|' + field_format.format(i)
             sep_line += '+' + '-' * 5
             count_line += '|' + field_format.format(c)
